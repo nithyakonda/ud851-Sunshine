d6da9898c333f1a2640e83cd58beb41d127c2383
Nkonda <nkonda@roamdata.com>
2018-01-31 00:03:56 -0500

    Finished S10.01-Exercise-SynchronizingTheWeather

--- S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -1,4 +1,43 @@
-//  TODO (1) Create a class called SunshineSyncTask
-//  TODO (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
-//      TODO (3) Within syncWeather, fetch new weather data
-//      TODO (4) If we have valid results, delete the old data and insert the new
+package com.example.android.sunshine.sync;
+
+import android.content.AsyncTaskLoader;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+
+import com.example.android.sunshine.data.WeatherContract;
+import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class SunshineSyncTask{
+    synchronized public static void syncWeather(Context context) {
+        try {
+            URL url = NetworkUtils.getUrl(context);
+            String responseJson = NetworkUtils.getResponseFromHttpUrl(url);
+
+            ContentValues[] weatherData = OpenWeatherJsonUtils
+                    .getWeatherContentValuesFromJson(context, responseJson);
+
+            if (weatherData != null && weatherData.length != 0) {
+                ContentResolver contentResolver = context.getContentResolver();
+                contentResolver.delete(
+                        WeatherContract.WeatherEntry.CONTENT_URI,
+                        null,
+                        null);
+
+                contentResolver.bulkInsert(
+                        WeatherContract.WeatherEntry.CONTENT_URI,
+                        weatherData);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
