b56af75b627147788241b468bd1807e420968880
Nkonda <nkonda@roamdata.com>
2018-01-31 23:34:46 -0500

    Finished S10.02-Exercise-SmarterSyncing

--- S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -15,20 +15,45 @@
  */
 package com.example.android.sunshine.sync;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
+
+import com.example.android.sunshine.data.WeatherContract;
 
 
 public class SunshineSyncUtils {
 
-//  TODO (1) Declare a private static boolean field called sInitialized
+    private static boolean sInitialized;
 
-    //  TODO (2) Create a synchronized public static void method called initialize
-    //  TODO (3) Only execute this method body if sInitialized is false
-    //  TODO (4) If the method body is executed, set sInitialized to true
-    //  TODO (5) Check to see if our weather ContentProvider is empty
-        //  TODO (6) If it is empty or we have a null Cursor, sync the weather now!
+    synchronized public static void initialize(final Context context) {
+        if (!sInitialized) {
+            sInitialized = true;
+            AsyncTask getWeatherTask = new AsyncTask() {
+                @Override
+                protected Object doInBackground(Object[] objects) {
+                    ContentResolver resolver = context.getContentResolver();
+                    return resolver.query(WeatherContract.WeatherEntry.CONTENT_URI,
+                            null,
+                            null,
+                            null,
+                            null);
+                }
+
+                @Override
+                protected void onPostExecute(Object o) {
+                    Cursor result = (Cursor) o;
+                    if (result == null || result.getCount() == 0) {
+                        startImmediateSync(context);
+                    }
+                }
+            };
+            getWeatherTask.execute();
+        }
+    }
 
     /**
      * Helper method to perform a sync immediately using an IntentService for asynchronous
